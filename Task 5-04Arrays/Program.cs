3518fb592aec595083fe60ae7b57c99459d9be57
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2022-12-22 03:27:01 +0100

    Add project files.

---
+++ Task 5-04Arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Hello World!!");
+
+while (true) 
+{
+    Console.Write("Enter the Value: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"Following are the prime numbers between 1 and {n}");
+    primeNumbers(n);
+    Console.WriteLine("\n");
+    
+}
+void primeNumbers(int n)
+{
+    bool[] prime = new bool[n + 1];
+
+    for (int i = 0; i < n; i++)
+        prime[i] = true;
+
+    for (int p = 2; p * p < n; p++)
+    {
+
+        if (prime[p] == true)
+        {
+            for (int i = p * p; i < n; i += p)
+                prime[i] = false;
+        }
+    }
+
+    // Print all prime numbers
+    for (int i = 2; i <= n; i++)
+    {
+        if (prime[i] == true)
+            Console.Write(i + " ");
+    }
+}
